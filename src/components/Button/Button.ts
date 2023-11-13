c8be89653d0bf25f0bd7e46b75d1288291fceafd
Dominykas Bartkus <dominykasbart@gmail.com>
2023-11-13 14:58:26 +0200

    feature(landing): styled basic landing page with root functionality

--- src/components/Button/Button.ts
+++ src/components/Button/Button.ts
@@ -1,35 +1,55 @@
+import tailwind from "../../base.css?inline";
+
 export class CustomButton extends HTMLElement {
   public static readonly componentName = "custom-button";
+  public static readonly extends = "button";
 
-  private readonly element = document.createElement("button");
+  private readonly node = document.createElement("button");
+  private readonly styleNode = document.createElement("style");
   private readonly shadow = this.attachShadow({ mode: "closed" });
   private class: string[] = [
     "px-4",
     "py-1",
-    "text-zinc-50",
+    "text-zinc-700",
     "border",
     "border-zinc-50",
     "rounded",
     "hover:bg-zinc-50",
-    "hover:text-zinc-800",
+    "hover:text-zinc-700",
   ];
 
   constructor() {
     super();
-    this.init();
     this.render();
+    this.init();
+  }
+
+  public connectedCallback() {
+    // event listeners
+    this.node.addEventListener("click", this.onClick);
+  }
+
+  public disconnectedCallback() {
+    // remove event listeners
+    this.node.removeEventListener("click", this.onClick);
   }
 
   protected init() {
-    this.element.classList.add(...this.class);
-    this.shadow.appendChild(this.element);
+    this.node.classList.add(...this.class);
+    this.styleNode.textContent = tailwind;
+    this.shadow.appendChild(this.styleNode);
+    this.shadow.appendChild(this.node);
   }
 
   protected render() {
-    this.element.innerHTML = `
+    this.node.innerHTML = `
       <span class="text-uppercase"> 
-        Click me
+        <slot></slot>
       </span>
     `;
+  }
+
+  private onClick() {
+    console.log("Get memed, son!");
   }
 }
